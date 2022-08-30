24bf2bd086ae5b7fb9413a28fd48f0db8c971914
Akram Pasha <akramap215@gmail.com>
2022-08-31 00:42:57 +0530

    Added monsterss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import { CardList } from "./components/card-list/card-list";
+import { SearchBox } from "./components/search-box/search-box";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+    
+  }
+
+  /* React Life cycles method*/
+  /* renders component onto DOM for first time  */
+  async componentDidMount() {
+    const users = await (
+      await fetch("https://jsonplaceholder.typicode.com/users")
+    ).json();
+    this.setState({ monsters: users });
+  }
+
+  handleClick3 = ()=> console.log("click3 ***");
+
+  handleClick1 = ()=> console.log("click1 *");
+  //  arrow function because it binds this to object where function is defined
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+  render() {
+    const { monsters, searchField } = this.state;
+    const filterMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <SearchBox
+          placeholder="search monster"
+          handleChange={this.handleChange}
+        />
+        <CardList searchKey={searchField} monsters={filterMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
